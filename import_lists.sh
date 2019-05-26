5fe1e4fe3ae77927cee1f2bedc3c344d0186253d
Mail Control <mailctl@mailctl1.vadm.csph.cloud>
2019-05-26 21:00:21 +0200

    import lists

---
+++ import_lists.sh
@@ -0,0 +1,18 @@
+#!/bin/ksh
+# 
+# This is a verry crude first example of how to import whitescan 
+# lists to a whitescan instand and how to export them 
+# 
+WHITESCAN=/root/whitescan.pl
+EXPORT=/root/spamd-whitescan-export/
+
+cd ${EXPORT}
+git pull || exit $?
+${WHITESCAN} -i ${EXPORT} || exit $?
+if [[ $1 == "push" ]]
+then
+        ${WHITESCAN} -e ${EXPORT} || exit $?
+        git commit -m "updated records" 
+        git push
+fi
+
